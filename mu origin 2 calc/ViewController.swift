0b3d3b59db20824fdf57d24f761b3eb34f50b9f6
Aleksandr Kornjushko <sasa330@gmail.com>
2020-12-09 12:46:58 +0200

    Initial Commit

---
+++ mu origin 2 calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mu origin 2 calc
+//
+//  Created by GOLDFNGER69 on 09.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
